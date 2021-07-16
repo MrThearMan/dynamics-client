03e91436f211e6c545722f81f1ae9fa880aa977b
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2021-07-16 16:28:11 +0300

    DateRange takes dates in as datetime objects instead of strings
    Also, membership test now catched exceptions for wrong input types

--- dynamics/utils.py
+++ dynamics/utils.py
@@ -1,6 +1,5 @@
-""""""
-
 import pytz
+import logging
 from typing import Tuple
 from datetime import datetime
 
@@ -14,15 +13,15 @@ __all__ = [
 ]
 
 
+logger = logging.getLogger(__name__)
+
+
 def to_dynamics_date_format(date: datetime, from_timezone: str = None) -> str:
     """Convert a datetime-object to a Dynamics compatible ISO formatted date string.
 
     :param date: Datetime object.
     :param from_timezone: Name of the timezone, from 'pytz.all_timezones', the date is in.
-                          You can also just add the tzinfo to the datetime object, but this
-                          can serve as a convenient place to do that. If you add the tzinfo
-                          yourself, use pytz.timezone(from_timezone).localize(date)
-                          to account for daylight savings time!
+                          Dynamics dates are in UCT, so timezoned values need to be converted to it.
     """
 
     if from_timezone is not None and date.tzinfo is None:
@@ -35,10 +34,10 @@ def to_dynamics_date_format(date: datetime, from_timezone: str = None) -> str:
     return date.replace(tzinfo=None).isoformat(timespec="seconds") + "Z"
 
 
-def from_dynamics_date_format(date: str, to_timezone: str = "UTC") -> datetime:
+def from_dynamics_date_format(date: str, to_timezone: str = "UCT") -> datetime:
     """Convert a Dynamics compatible ISO formatted date string to a datetime-object.
 
-    :param date: ISO datestring from Dynamics database, in form: YYYY-mm-ddTHH:MM:SSZ.
+    :param date: Date string in form: YYYY-mm-ddTHH:MM:SSZ
     :param to_timezone: Name of the timezone, from 'pytz.all_timezones', to convert the date to.
                         This won't add 'tzinfo', instead the actual time part will be changed from UTC
                         to what the time is at 'to_timezone'.
@@ -56,46 +55,35 @@ class DateRange:
     but note that this only takes into account the date, and not the time part.
     """
 
-    def __init__(self, start: str = None, end: str = None, start_key: str = None, end_key: str = None):
+    def __init__(self, start: datetime = None, end: datetime = None, start_key: str = None, end_key: str = None):
 
         if start and not start_key:
             raise ValueError("Dynamics table date start key needed if start defined.")
         if end and not end_key:
             raise ValueError("Dynamics table date end key needed if end defined.")
 
-        self.now_date = datetime.now()
-        self.now_string = to_dynamics_date_format(self.now_date)
-
         self.filter_range = []
-        self.start_date = None
-        self.end_date = None
-        self.start_string = start
-        self.end_string = end
+        self.start_date = start
+        self.end_date = end
+        self.start_string = ""
+        self.end_string = ""
 
-        if start == "now":
-            self.start_date = self.now_date
-            self.filter_range.append(ftr.on_or_after(end_key, self.now_string))
-        elif start is not None:
-            try:
-                self.start_date = from_dynamics_date_format(start)
-                self.filter_range.append(ftr.on_or_after(end_key, start))
-            except ValueError:
-                pass  # Date format incorrect, read as empty
+        if start is not None:
+            self.start_string = to_dynamics_date_format(start)
+            self.filter_range.append(ftr.on_or_after(end_key, self.start_string))
 
-        if end == "now":
-            self.end_date = self.now_date
-            self.filter_range.append(ftr.on_or_before(start_key, self.now_string))
-        elif end is not None:
-            try:
-                self.end_date = from_dynamics_date_format(end)
-                self.filter_range.append(ftr.on_or_before(start_key, end))
-            except ValueError:
-                pass  # Date format incorrect, read as empty
+        if end is not None:
+            self.end_string = to_dynamics_date_format(end)
+            self.filter_range.append(ftr.on_or_before(start_key, self.end_string))
 
     def __contains__(self, item: Tuple[str, str]) -> bool:
         """Check if (start, end) dynamics ISO formatted strings are in the defined range."""
-        booking_start = from_dynamics_date_format(item[0])
-        booking_end = from_dynamics_date_format(item[1])
+        try:
+            booking_start = from_dynamics_date_format(item[0])
+            booking_end = from_dynamics_date_format(item[1])
+        except Exception:  # noqa
+            logger.error(f"Bad date values, either '{item[0]}' or '{item[1]}'.")
+            return False
 
         if self.start_date and booking_end < self.start_date:
             return False
