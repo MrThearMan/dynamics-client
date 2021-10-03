c19d97c4fb6862009ea264810b0091c1c4491f07
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2021-10-03 23:08:31 +0300

    Add initial pytest fixture configuration.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,92 @@
-import os
+from contextlib import contextmanager
+from unittest import mock
 
 import pytest
+
+from dynamics.client import DynamicsClient
+from dynamics.typing import Literal, Union
+
+
+__all__ = [
+    "DynamicsResponse",
+    "dynamics_client_response",
+]
+
+
+@pytest.fixture
+def dynamics_client(request, _dynamics_client_constructor) -> DynamicsClient:
+    if not hasattr(request, "param"):
+        yield _dynamics_client_constructor
+        return
+
+    with dynamics_client_response(
+        _dynamics_client_constructor,
+        method=request.param[0],
+        response=request.param[1],
+        status_code=request.param[2],
+    ) as client:
+        yield client
+
+
+class DynamicsResponse:
+    """Used in pytest.mark.parametrize to pass used client method,
+    response data, and status code to the mocker fixture in a more explicit way.
+    """
+
+    def __init__(
+        self,
+        *,
+        method: Literal["get", "post", "patch", "delete"],
+        response: Union[list, dict, None],
+        status_code: int,
+    ):
+        self.content = [method, response, status_code]
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, item):
+        return self.content[item]
+
+    def __next__(self):
+        return next(self.content)
+
+
+@contextmanager
+def dynamics_client_response(
+    client: DynamicsClient,
+    *,
+    method: Literal["get", "post", "patch", "delete"],
+    response: Union[list, dict, None],
+    status_code: int,
+) -> DynamicsClient:
+    """Mock dynamics client response for certain method."""
+    with mock.patch(
+        f"requests_oauthlib.oauth2_session.OAuth2Session.{method}",
+        new_callable=_request_call(response, status_code),
+    ):
+        yield client
+
+
+class _RequestMock:
+    def __init__(self, params, status_code):
+        self.params = params
+        self.status_code = status_code
+
+    def json(self):
+        return self.params
+
+
+def _request_call(response, status_code):
+    def request_mock(self, url, **kwargs):
+        return _RequestMock(response, status_code)
+
+    return lambda: request_mock
+
+
+@pytest.fixture(scope="session")
+def _dynamics_client_constructor():
+    with mock.patch("requests_oauthlib.oauth2_session.OAuth2Session.fetch_token", new_callable=mock.PropertyMock):
+        with mock.patch("dynamics.utils.SQLiteCache.set", new_callable=mock.PropertyMock):
+            dynamics_client = DynamicsClient("", "", "", "", [])
+            yield dynamics_client
--- tests/test_dynamics_client.py
+++ tests/test_dynamics_client.py
@@ -1,2 +1,32 @@
-def test_pass():
-    pass
+import pytest
+
+from dynamics.client import DynamicsClient
+
+from .conftest import DynamicsResponse, dynamics_client_response
+
+
+@pytest.mark.parametrize(
+    "dynamics_client",
+    [
+        DynamicsResponse(method="get", response={"foo": "bar", "one": 2}, status_code=200),
+    ],
+    indirect=True,
+)
+def test_get(dynamics_client: "DynamicsClient"):
+    assert dynamics_client.get() == [{"foo": "bar", "one": 2}]
+
+
+@pytest.mark.parametrize(
+    "dynamics_client",
+    [
+        DynamicsResponse(method="post", response={"foo": "bar", "one": 2}, status_code=200),
+    ],
+    indirect=True,
+)
+def test_post(dynamics_client: "DynamicsClient"):
+    assert dynamics_client.post(data={"foo": "bar", "one": 2}) == {"foo": "bar", "one": 2}
+
+
+def test_no_parameters(dynamics_client: "DynamicsClient"):
+    with dynamics_client_response(dynamics_client, method="post", response={}, status_code=200):
+        assert dynamics_client.post(data={"foo": "bar", "one": 2}) == {}
